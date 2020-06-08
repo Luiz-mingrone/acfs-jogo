60c389751f687c8fce3ee873b44025574f7b3cf6
Luiz-mingrone <rickmingrone@gmail.com>
2020-06-08 19:28:39 -0300

    jogo

---
+++ New Unity Project (2)/Assets/objeto.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objeto : MonoBehaviour {
+
+    public GameObject cubo;
+	
+	// Update is called once per frame
+	void Update () {
+		if (cubo.transform.position.z > transform.position.z + 30)
+        {
+            Destroy(this);
+        }
+	}
+}
---
+++ New Unity Project (2)/Assets/plano1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plano1 : MonoBehaviour {
+    public GameObject cubo;
+    public GameObject ob;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(cubo.transform.position.z > this.transform.position.z+40)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z+180);
+            
+
+            Instantiate(ob, new Vector3(Random.Range(-6, 7), 2.1f, Random.Range(transform.position.z - 30, transform.position.z + 30)), Quaternion.identity);
+            Instantiate(ob, new Vector3(Random.Range(-6, 7), 2.1f, Random.Range(transform.position.z - 30, transform.position.z + 30)), Quaternion.identity);
+            Instantiate(ob, new Vector3(Random.Range(-6, 7), 2.1f, Random.Range(transform.position.z - 30, transform.position.z + 30)), Quaternion.identity);
+        }
+    }
+    }
+
+	
+
