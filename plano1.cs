2432e2bc13f9a8423ce1c04399b24dfa90dc78f4
LarissaSilveiraBonifacio <larissa.silveira02@gmail.com>
2020-06-16 10:57:22 -0300

    Update plano1.cs

--- plano1.cs
+++ plano1.cs
@@ -1,29 +1,55 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine;
 
-public class plano1 : MonoBehaviour {
-    public GameObject cubo;
-    public GameObject ob;
-	//tem 3 plataformas que quando o carro passa por elas, são transportadas para frente das outras formando um loop
-	void Start () {
-		
-	}
-	
-	
-	void Update () {
-        //depois que o carro passar por esta plataforma ela será transportada para 180 posições pra frente(aparece atras das outras duas plataformas criando um loop)
-		if(cubo.transform.position.z > this.transform.position.z+40)
-        {
+ public class plano1 : MonoBehaviour {
+     public GameObject cubo;
+     public GameObject ob;
+      public bool criarObjeto;
+    
+ 	//tem 3 plataformas que quando o carro passa por elas, são transportadas para frente das outras formando um loop
+void Start () {
+     
+}
+
+ IEnumerator Espera (){
+             
+      criarObjeto = false;
+       yield return new WaitForSeconds(10.0f);
+      
+
+ }
+
+
+void   Update () {
+        
+         //depois que o carro passar por esta plataforma ela será transportada para 180 posições pra frente(aparece atras das outras duas plataformas criando um loop)
+ 		 if(cubo.transform.position.z > this.transform.position.z+40 && criarObjeto = true )
+         {
             transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z+180);
-            
-            //cria 3 objetos em posições aleatorias dentro do espaço da plataforma
-            Instantiate(ob, new Vector3(Random.Range(-6, 7), 2.1f, Random.Range(transform.position.z - 30, transform.position.z + 30)), Quaternion.identity);
-            Instantiate(ob, new Vector3(Random.Range(-6, 7), 2.1f, Random.Range(transform.position.z - 30, transform.position.z + 30)), Quaternion.identity);
-            Instantiate(ob, new Vector3(Random.Range(-6, 7), 2.1f, Random.Range(transform.position.z - 30, transform.position.z + 30)), Quaternion.identity);
-        }
-    }
-    }
+                    
+             //cria 3 objetos em posições aleatorias dentro do espaço da plataforma
+             Instantiate(ob, new Vector3(1f, 2.1f, Random.Range(transform.position.z - 40, transform.position.z + 60)), Quaternion.identity);
+                  StartCoroutine("Espera");
+             Instantiate(ob, new Vector3(1f, 2.1f, Random.Range(transform.position.z - 30, transform.position.z + 30)), Quaternion.identity);
+               StartCoroutine("Espera");
+            Instantiate(ob, new Vector3(1f, 2.1f, Random.Range(transform.position.z - 30, transform.position.z + 30)), Quaternion.identity);
+        
+         }
+         if(cubo.transform.position.z < this.transform.position.z-40 && criarObjeto == true )
+             {
+                transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z-180);
+             
+                //cria 3 objetos em posições aleatorias dentro do espaço da plataforma
+                Instantiate(ob, new Vector3(1f, 2.1f, Random.Range(transform.position.z - 40, transform.position.z + 60)), Quaternion.identity);
+                  StartCoroutine("Espera");
+                 Instantiate(ob, new Vector3(1f, 2.1f, Random.Range(transform.position.z - 30, transform.position.z + 30)), Quaternion.identity);
+                   StartCoroutine("Espera");
+                 Instantiate(ob, new Vector3(1f, 2.1f, Random.Range(transform.position.z - 30, transform.position.z + 30)), Quaternion.identity);
+ }
+ }
+
+ }
 
 	
 
