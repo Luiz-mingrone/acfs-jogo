cfd3c59e95c861aa4a2002c9b260b3cf8e833f7a
LarissaSilveiraBonifacio <larissa.silveira02@gmail.com>
2020-06-16 10:56:07 -0300

    Update objeto.cs

--- objeto.cs
+++ objeto.cs
@@ -1,18 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine;
 
-public class objeto : MonoBehaviour {
+ public class objeto : MonoBehaviour {
 
-    public GameObject cubo;
-	
-	
-	void Update () {
-        //ainda não está funcionando
-        //se o carro estiver 30 posições a frente do objeto ele sera destruido
-		if (cubo.transform.position.z > transform.position.z + 30)
-        {
-            Destroy(this);
-        }
-	}
-}
+     public GameObject cubo;
+
+
+ 	void Update () {
+         //ainda não está funcionando
+         //se o carro estiver 30 posições a frente do objeto ele sera destruido
+ 		if (cubo.transform.position.z > transform.position.z + 30)
+         {
+             Destroy(this);
+         }
+    
+ 	}
+ }
