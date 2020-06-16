5ae8efcd1a63e5cd9ab99c788d5fa0789cfe57bd
LarissaSilveiraBonifacio <larissa.silveira02@gmail.com>
2020-06-16 10:58:10 -0300

    Update run.cs

--- run.cs
+++ run.cs
@@ -1,85 +1,86 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine;
 
-public class run : MonoBehaviour
-{
-    public GameObject cubo;
+ public class run : MonoBehaviour
+ {
+     public GameObject cubo;
 
-    public float speed = 100f;
-    private CharacterController characterController;
-    private float jumpforce = 10f;
-    private float gravidade = 30f;
-    private Vector3 direcao = Vector3.zero;
-   
-
-
-    private void Update()
-    {
-        CharacterController controller = gameObject.GetComponent<CharacterController>();
-
-        if (controller.isGrounded)
-        {
-            direcao = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            direcao = transform.TransformDirection(direcao);
-            direcao *= speed;
+     public float speed = 100f;
+     private CharacterController characterController;
+     private float jumpforce = 10f;
+     private float gravidade = 30f;
+     private Vector3 direcao = Vector3.zero;
 
 
-        }
+
+     private void Update()
+     {
+         CharacterController controller = gameObject.GetComponent<CharacterController>();
+
+         if (controller.isGrounded)
+         {
+             direcao = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+             direcao = transform.TransformDirection(direcao);
+             direcao *= speed;
+            
+
+         }
 
 
-        if (Input.GetButton("Jump"))
-        {
-            direcao.y = jumpforce;
+         if (Input.GetButton("Jump"))
+         {
+             direcao.y = jumpforce;
+             direcao.z = 5f;
+             speed = 5f;
+             
 
-        }
-
-
-        //gravidade
-        direcao.y -= gravidade * Time.deltaTime;
-
-        //andar
-        controller.Move(direcao * Time.deltaTime);
+         }
 
 
-        //limitador de altura
-        if (transform.position.y > 4)
-        {
-            transform.position = new Vector3(transform.position.x, 4, transform.position.z);
-        }
-        //limitadores do eixo x (lados)
-        if (transform.position.x<= -6.82f)
-        {
-            transform.position = new Vector3(-6.82f,transform.position.y,transform.position.z);
-        }
-        if (transform.position.x >= 6.82f)
-        {
-            transform.position = new Vector3(6.82f, transform.position.y, transform.position.z);
-        }
+         //gravidade
+         direcao.y -= gravidade * Time.deltaTime;
 
-        //reiniciar ao cair
-
-        if (transform.position.y < -4)
-        {
-
-            transform.position = new Vector3(0, 2, transform.position.z);
-            //speed = 0;
-            //yield return new WaitForSeconds(1);
-            //StartCoroutine(Cair());
-        }
+         //andar
+         controller.Move(direcao * Time.deltaTime);
 
 
-    }
-    //ignorar por enquanto
-    IEnumerator Cair()
-    {
+         //limitador de altura
+         if (transform.position.y > 4)
+         {
+             transform.position = new Vector3(transform.position.x, 4, transform.position.z);
+         }
+         //limitadores do eixo x (lados)
+         if (transform.position.x<= -6.82f)
+         {
+             transform.position = new Vector3(-6.82f,transform.position.y,transform.position.z);
+         }
+         if (transform.position.x >= 6.82f)
+         {
+             transform.position = new Vector3(6.82f, transform.position.y, transform.position.z);
+         }
 
-        speed = 0;
-        jumpforce = 0;
-        yield return new WaitForSeconds(1);
-        speed = 10;
-        jumpforce = 10;
+         //reiniciar ao cair
 
-    }
+         if (transform.position.y < -4)
+         {
 
-}
+             transform.position = new Vector3(0, 2, transform.position.z);
+            
+         }
+
+
+     }
+     //ignorar por enquanto
+     IEnumerator Cair()
+     {
+
+         speed = 0;
+         jumpforce = 0;
+         yield return new WaitForSeconds(1);
+         speed = 10;
+         jumpforce = 10;
+
+     }
+
+ }
